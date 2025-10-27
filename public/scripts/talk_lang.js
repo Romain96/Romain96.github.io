3d535059cfc99dcc554ea066fb0ff2885b7b1668
Romain PERRIN <romain.perrin@etu.unistra.fr>
2025-10-27 11:39:53 +0100

    adding languages to talks page

---
+++ public/scripts/talk_lang.js
@@ -0,0 +1,66 @@
+let prefLang = localStorage.getItem('language') || 'en';
+
+if (prefLang == 'fr')
+{
+	var selector = document.querySelector(".languageSelector");
+	selector.value = 'fr';
+}
+else
+{
+	var selector = document.querySelector(".languageSelector");
+	selector.value = 'en';
+}
+
+function changeLanguageListener(value)
+{
+	if (value == "en")
+	{
+		changeLanguage('en');
+	}
+	else if (value == "fr")
+	{
+		changeLanguage('fr');
+	}
+	else
+	{
+		changeLanguage('en');
+	} 
+}
+
+function updateContent(langData)
+{
+	document.querySelectorAll('[data-i18n]').forEach(
+		element => 
+		{
+			const key = element.getAttribute('data-i18n');
+			element.innerHTML = langData[key];
+		}
+	);
+}
+
+function setLanguagePreference(lang)
+{
+	localStorage.setItem('language', lang);
+	location.reload();
+}
+
+async function fetchLanguageData(lang)
+{
+	const response = await fetch(`public/languages/talk_${lang}.json`);
+	return response.json();
+}
+
+async function changeLanguage(lang)
+{
+	setLanguagePreference(lang);
+	const langData = fetchLanguageData(lang);
+	updateContent(langData);
+}
+
+window.addEventListener('DOMContentLoaded', async () => 
+	{
+		const userPreferedLanguage = localStorage.getItem('language') || 'en';
+		const langData = await fetchLanguageData(userPreferedLanguage);
+		updateContent(langData);
+	}
+);
